44f4a6da8eb573fb180773dad785f9ef56cd8064
Dominic Dahnelt <dominic.dahnelt@gmail.com>
2017-04-10 16:42:57 +0200

    # fixed bug with label; renamed input-value

--- todomanager/src/pages/tasks/tasks.ts
+++ todomanager/src/pages/tasks/tasks.ts
@@ -8,15 +8,22 @@ import { ToastController } from 'ionic-angular';
 })
 
 export class TasksPage {
-  value: Text;
+  todoLabel: Text;
   constructor(
     public navCtrl: NavController,
     public toastCtrl: ToastController) {
   }
   showToast() {
-    console.log(this.value);
+    let msg = "";
+    if(this.todoLabel === undefined)
+    {
+      msg = "Please enter a title"
+    }
+    else{
+      msg = "TODO added " + this.todoLabel;
+    }
     let toast = this.toastCtrl.create({
-      message: 'TODO added ' + this.value,
+      message: msg,
       duration: 5000
     });
     toast.present();
